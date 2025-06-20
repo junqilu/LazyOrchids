c080f92f2fee0afdbae2e1c4bfc29d9d1775f0d4
Junqi Lu <40476820+junqilu@users.noreply.github.com>
2025-06-19 21:14:18 -0400

    Update sensor.ino

--- Supervisor/sensor.ino
+++ Supervisor/sensor.ino
@@ -1,7 +1,7 @@
-#include <Adafruit_SHT31.h>
 #include "sensor.h"
 
 static Adafruit_SHT31 sht = Adafruit_SHT31();
+static bool first_sensor_upload = true;
 
 bool setup_sensor() {
   if (!sht.begin(0x44)) { // 0x44 is the default I2C address
@@ -31,5 +31,33 @@ bool read_sensor(SensorData &sensor_data) {
     Serial.println("Sensor reading failed (NaN values)");
     return false;
   }
-  
 }
+
+bool data_upload_sensor(NonBlockingTimer &sensor_timer, int sensor_field_temperature, int sensor_field_humidity){
+  if (first_sensor_upload || sensor_timer.is_ready()) {
+    SensorData sensor_data;
+
+    if (read_sensor(sensor_data)) {
+      ThingSpeak.setField(sensor_field_temperature, sensor_data.temperature);
+      ThingSpeak.setField(sensor_field_humidity, sensor_data.humidity);
+      int result = ThingSpeak.writeFields(SECRET_CH_ID, SECRET_WRITE_APIKEY);
+
+      if (result == 200) {
+        Serial.println("Sensor data sent to ThingSpeak.");
+      } else {
+        Serial.print("Error sending sensor data: ");
+        Serial.println(result);
+      }
+
+      if (first_sensor_upload) {
+        sensor_timer.reset(); 
+        first_sensor_upload = false;
+      }
+    }
+    return true; 
+  }else {
+    return false;
+  }
+}
+
+
