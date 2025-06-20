3361bfb12d56b320d930cf28aa4a3062d190924e
Junqi Lu <40476820+junqilu@users.noreply.github.com>
2025-06-19 21:13:53 -0400

    Update ldr.ino

--- Supervisor/ldr.ino
+++ Supervisor/ldr.ino
@@ -1,11 +1,15 @@
-#include <Arduino.h>
 #include "ldr.h"
 
-bool read_ldr(int light_pin, LightData &data) {
+
+static bool first_ldr_upload = true;
+
+
+
+bool read_ldr(int light_pin, LightData &light_data) {
   int raw_light = analogRead(light_pin);  // Read analog value from LDR
 
   if (!isnan(raw_light)){
-    data.light_reading = raw_light;
+    light_data.light_reading = raw_light;
 
     Serial.print("Light reading: ");
     Serial.println(raw_light);
@@ -13,6 +17,32 @@ bool read_ldr(int light_pin, LightData &data) {
     return true; // Always returns true; could add error checking later
   }else{
     Serial.println("Light reading failed (NaN values)");
+    return false;
+  }
+}
+
+bool data_upload_ldr(int light_pin, NonBlockingTimer &ldr_timer, int ldr_field_light) {
+  if (first_ldr_upload || ldr_timer.is_ready()) {
+    LightData light_data;
+
+    if (read_ldr(light_pin, light_data)) {
+      ThingSpeak.setField(ldr_field_light, light_data.light_reading);
+      int result = ThingSpeak.writeFields(SECRET_CH_ID, SECRET_WRITE_APIKEY);
+
+      if (result == 200) {
+        Serial.println("Light data sent to ThingSpeak.");
+      } else {
+        Serial.print("Error sending light data: ");
+        Serial.println(result);
+      }
+
+      if (first_ldr_upload) {
+        ldr_timer.reset();
+        first_ldr_upload = false;
+      }
+    }
+    return true;
+  }else {
     return false;
   }
 }
