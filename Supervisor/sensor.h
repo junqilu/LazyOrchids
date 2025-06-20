50b6657a87df706a8aa8d9b76c99d8d542a4290b
Junqi Lu <40476820+junqilu@users.noreply.github.com>
2025-06-19 21:14:05 -0400

    Update sensor.h

--- Supervisor/sensor.h
+++ Supervisor/sensor.h
@@ -1,6 +1,11 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+#include <Adafruit_SHT31.h>
+#include <ThingSpeak.h>
+#include "secrets.h"
+#include "timer.h"
+
 struct SensorData {
   float temperature;
   float humidity; 
@@ -8,6 +13,7 @@ struct SensorData {
 
 bool setup_sensor();
 bool read_sensor(SensorData &sensor_data);
+bool data_upload_sensor(NonBlockingTimer &sensor_timer, int sensor_field_temperature, int sensor_field_humidity);
 
 
 #endif
