0f4c955036505189beba10b02b0268a130205be5
Junqi Lu <40476820+junqilu@users.noreply.github.com>
2025-06-19 21:13:40 -0400

    Update ldr.h

--- Supervisor/ldr.h
+++ Supervisor/ldr.h
@@ -1,10 +1,18 @@
-#ifndef LDR_CONTROL_H
-#define LDR_CONTROL_H
+#ifndef LDR_H
+#define LDR_H
+
+#include <Arduino.h>
+#include <ThingSpeak.h>
+#include "secrets.h"
+#include "timer.h"
 
 struct LightData {
   int light_reading;
 };
 
-bool read_ldr(int light_pin, LightData &data);
+
+bool read_ldr(int light_pin, LightData &light_data);
+bool data_upload_ldr(int light_pin, NonBlockingTimer &ldr_timer, int ldr_field_light); 
+
 
 #endif
