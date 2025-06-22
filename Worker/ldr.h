1d40ca3c05c3ac728ea978d66c600c4f02c90b41
Jason Lu <jql2026@gmail.com>
2025-06-21 20:35:51 -0400

    moved in all the global variables for setting up the treatment plan

--- Worker/ldr.h
+++ Worker/ldr.h
@@ -14,4 +14,25 @@ struct TreatmentPlan {
 
 bool get_treatment_plan(int light_pin, TreatmentPlan &treatment_plan);
 
+TreatmentPlan treatment_plan = {true, false, 85, 90, 20UL * 60UL * 1000UL, 3UL * 60UL * 1000UL}; // default settings
+unsigned long last_light_check = 0;
+extern const unsigned long light_check_interval_seconds; 
+const unsigned long light_check_interval = light_check_interval_seconds * 1000UL;
+
+// Refer to the global variables provided in the Worker.ino
+extern const int day_humidity_min;
+extern const int day_humidity_max;
+extern const unsigned long day_fan_interval_min;
+extern const unsigned long day_fan_duration_min;
+
+extern const int day_night_light_threshold;
+
+extern const int night_humidity_min;
+extern const int night_humidity_max;
+extern const unsigned long night_fan_interval_min;
+extern const unsigned long night_fan_duration_min;
+
+extern const int night_sleep_light_threshold;
+
+
 #endif
